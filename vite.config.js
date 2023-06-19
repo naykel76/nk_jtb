fe57084ae273704e26c708bcaa98604c1d673a0d
Nathan <nathan@naykel.com.au>
2023-06-20 04:48:24 +1000

    update vars

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,20 @@ export default defineConfig({
                 autoprefixer({}),
                 purgecss({
                     content: ['./index.html'],
+                    // blocklist: ['usedClass', /^nav-/],
+                    // https://github.com/FullHuman/purgecss/issues/978
+                    safelist: {
+                        standard: [/^\:has$/, /^\:is$/, /^\:not$/, /^\:where$/]
+                    },
+                    extractors: [
+                        {
+                            extractor: (content) => {
+                                // fix for escaped prefixes (sm:, lg:, etc)
+                                return content.match(/[A-Za-z0-9-_:\/]+/g) || []
+                            },
+                            extensions: ['css', 'html', 'vue'],
+                        },
+                    ],
                 }),
             ],
         }
