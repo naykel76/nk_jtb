25b2ffa38974ef558985ce30b6ddbcee8bc97806
Nathan <nathan@naykel.com.au>
2023-06-28 11:31:04 +1000

    update themes and colors

--- vite.config.js
+++ vite.config.js
@@ -8,23 +8,23 @@ export default defineConfig({
         postcss: {
             plugins: [
                 autoprefixer({}),
-                purgecss({
-                    content: ['./index.html'],
-                    // blocklist: ['usedClass', /^nav-/],
-                    // https://github.com/FullHuman/purgecss/issues/978
-                    safelist: {
-                        standard: [/^\:has$/, /^\:is$/, /^\:not$/, /^\:where$/]
-                    },
-                    extractors: [
-                        {
-                            extractor: (content) => {
-                                // fix for escaped prefixes (sm:, lg:, etc)
-                                return content.match(/[A-Za-z0-9-_:\/]+/g) || []
-                            },
-                            extensions: ['css', 'html', 'vue'],
-                        },
-                    ],
-                }),
+                // purgecss({
+                //     content: ['./index.html'],
+                //     // blocklist: ['usedClass', /^nav-/],
+                //     // https://github.com/FullHuman/purgecss/issues/978
+                //     safelist: {
+                //         standard: [/^\:has$/, /^\:is$/, /^\:not$/, /^\:where$/]
+                //     },
+                //     extractors: [
+                //         {
+                //             extractor: (content) => {
+                //                 // fix for escaped prefixes (sm:, lg:, etc)
+                //                 return content.match(/[A-Za-z0-9-_:\/]+/g) || []
+                //             },
+                //             extensions: ['css', 'html', 'vue'],
+                //         },
+                //     ],
+                // }),
             ],
         }
     }
