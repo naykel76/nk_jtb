e7facd29e85567ec7bdf141899f7505063630102
Nathan Watts <nathan@naykel.com.au>
2024-01-19 06:19:51 +1000

    add basic utility build

--- vite.config.js
+++ vite.config.js
@@ -4,29 +4,29 @@ import purgecss from '@fullhuman/postcss-purgecss';
 
 export default defineConfig({
 
-    css: {
-        postcss: {
-            plugins: [
-                autoprefixer({}),
-                purgecss({
-                    content: ['./**/*.html'],
-                    // content: ['./index.html'],
-                    // blocklist: ['usedClass', /^nav-/],
-                    // https://github.com/FullHuman/purgecss/issues/978
-                    // safelist: {
-                    //     standard: [/^\:has$/, /^\:is$/, /^\:not$/, /^\:where$/]
-                    // },
-                    // extractors: [
-                    //     {
-                    //         extractor: (content) => {
-                    //             // fix for escaped prefixes (sm:, lg:, etc)
-                    //             return content.match(/[A-Za-z0-9-_:\/]+/g) || []
-                    //         },
-                    //         extensions: ['css', 'html', 'vue'],
-                    //     },
-                    // ],
-                }),
-            ],
-        }
-    }
+    // css: {
+    //     postcss: {
+    //         plugins: [
+    //             autoprefixer({}),
+    //             purgecss({
+    //                 content: ['./**/*.html'],
+    //                 // content: ['./index.html'],
+    //                 // blocklist: ['usedClass', /^nav-/],
+    //                 // https://github.com/FullHuman/purgecss/issues/978
+    //                 // safelist: {
+    //                 //     standard: [/^\:has$/, /^\:is$/, /^\:not$/, /^\:where$/]
+    //                 // },
+    //                 // extractors: [
+    //                 //     {
+    //                 //         extractor: (content) => {
+    //                 //             // fix for escaped prefixes (sm:, lg:, etc)
+    //                 //             return content.match(/[A-Za-z0-9-_:\/]+/g) || []
+    //                 //         },
+    //                 //         extensions: ['css', 'html', 'vue'],
+    //                 //     },
+    //                 // ],
+    //             }),
+    //         ],
+    //     }
+    // }
 })
