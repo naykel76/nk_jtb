8823de973843f3a8cc8eb282e9c7da418535760f
Nathan Watts <nathan@naykel.com.au>
2025-04-14 10:51:12 +1000

    update dependencies and house keeping

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,5 @@
 import { defineConfig } from 'vite';
-import autoprefixer from 'autoprefixer'
-import purgecss from '@fullhuman/postcss-purgecss';
 
 export default defineConfig({
 
-    // css: {
-    //     postcss: {
-    //         plugins: [
-    //             autoprefixer({}),
-    //             purgecss({
-    //                 content: ['./**/*.html'],
-    //                 // content: ['./index.html'],
-    //                 // blocklist: ['usedClass', /^nav-/],
-    //                 // https://github.com/FullHuman/purgecss/issues/978
-    //                 // safelist: {
-    //                 //     standard: [/^\:has$/, /^\:is$/, /^\:not$/, /^\:where$/]
-    //                 // },
-    //                 // extractors: [
-    //                 //     {
-    //                 //         extractor: (content) => {
-    //                 //             // fix for escaped prefixes (sm:, lg:, etc)
-    //                 //             return content.match(/[A-Za-z0-9-_:\/]+/g) || []
-    //                 //         },
-    //                 //         extensions: ['css', 'html', 'vue'],
-    //                 //     },
-    //                 // ],
-    //             }),
-    //         ],
-    //     }
-    // }
 })
