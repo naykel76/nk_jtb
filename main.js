9557a1ea3299a77141f27f841a93171d055ce865
Nathan Watts <nathan@naykel.com.au>
2025-11-25 13:31:12 +1000

    feat: Add markdown loading and rendering functionality
    - Updated package.json to include dependencies for Alpine.js, Highlight.js, and Marked.
    - Added new SVG icon for "naykel" in icons.svg.
    - Created markdown-loader.js to handle loading and displaying markdown files with syntax highlighting.
    - Implemented a custom renderer in markdown-renderer.js to support collapsible code blocks and live previews.
    - Added navigation structure in nav.json for documentation sections and links.
    - Developed navigation.js to dynamically load and display the navigation based on nav.json, with event listeners for document loading.
    - Configured Vite to optimize build with chunking for highlight.js and set a warning limit for chunk sizes.

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
 import './src/build.scss'
 import './src/play.scss'
+import './src/extras/_nk-docs.scss'
+import 'highlight.js/styles/github-dark.css'
 
-// document.querySelector('#app').innerHTML = `Hello`
+import Alpine from 'alpinejs'
+import collapse from '@alpinejs/collapse'
+
+import './scripts/markdown-renderer.js'
+import './scripts/markdown-loader.js'
+import './scripts/navigation.js'
+
+Alpine.plugin(collapse)
+
+// Initialize Alpine.js
+window.Alpine = Alpine
+Alpine.start()
+
+
 
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,20 @@ import { defineConfig } from 'vite';
 export default defineConfig({
     server: {
         open: '/dev.html'
+    },
+    build: {
+        chunkSizeWarningLimit: 1000,
+        rollupOptions: {
+            output: {
+                manualChunks: (id) => {
+                    if (id.includes('node_modules')) {
+                        if (id.includes('highlight.js')) {
+                            return 'highlight';
+                        }
+                        return 'vendor';
+                    }
+                }
+            }
+        }
     }
 });
